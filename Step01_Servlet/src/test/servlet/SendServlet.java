9c65ba77842d65ea09c0db8a6c132c9ebb46160d
MSH <sehyun37@naver.com>
2022-02-07 10:16:33 +0900

    1

---
+++ Step01_Servlet/src/test/servlet/SendServlet.java
@@ -0,0 +1,41 @@
+package test.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/send")
+public class SendServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		/*
+		 * <input type="text" name="msg" placeholder="서버에 할 말 입력..."/>
+		 * 		 * 위의 input 요소에 입력한
+		 * msg라는 이름으로 전송된 문자열 추가하기
+		 */
+		String a=req.getParameter("msg");
+		System.out.println(a);
+		 
+		//응답 인코딩 설정
+		resp.setCharacterEncoding("utf-8");
+		//응답 컨텐츠 type 설정(웹브라우저에게 html 형식의 문자열을 응답할 
+		resp.setContentType("text/html; charset=utf-8");
+		//html 형식의 문자열 응답하기
+		PrintWriter pw = resp.getWriter();
+		pw.println("<!doctype html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<meta charset='utf-8'>");
+		pw.println("<title></title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("okay~");
+		pw.println("</body>");
+		pw.println("</html>");
+		pw.close();
+	}
+}
